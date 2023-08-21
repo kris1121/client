47de93c6ee9fba7c334c63736aa1a42fba2df5ad
Krzysztof Matyga <krzysztof.matyga@noinputsignal.com>
2023-08-21 17:16:51 +0200

    home, login, register pages, react router, menu and jumbotron aded

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Menu from "./components/nav/Menu";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Menu />
+      <Routes>
+        <Route path="/" element={ <Home /> }/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/cards/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Jumbotron = () => {
+  return (
+    <div className='container bg-primary'>
+      <div className='row'>
+        <div className='col text-center p-4 bg-light'>
+          <h1>Page title</h1>
+          <p className='lead'>Page subtitle</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Jumbotron
